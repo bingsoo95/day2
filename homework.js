dc76d2237548dc64e98a58adbd3a31001f996f3b
bingsoo95 <kimminsu403@naver.com>
2021-11-10 19:54:51 +0900

    day2

---
+++ homework.js
@@ -0,0 +1,6 @@
+const arr = [
+    {month:1, total:90}, {month:2, total:87}, {month:3,total:140},
+    {month:4, total:120}, {month:5, total:130}, {month:6,total:150},
+    {month:7, total:180}, {month:8, total:240}, {month:9,total:200},
+    {month:10, total:180}, {month:11, total:140}, {month:12,total:190}
+]
